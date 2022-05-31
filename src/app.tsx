6cf09068b12d77ccff340690fb8dbc51d9dff646
brock ross <brock.ross@dutchie.com>
2022-05-30 21:50:33 -0600

    set up react/ts/parcel

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <h1>b r o x l e x i c o n</h1>;
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./app";
+
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
