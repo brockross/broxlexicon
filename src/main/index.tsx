4ee97e136d77632a00bfd678aaa208834d1ff942
brock ross <brock.ross@dutchie.com>
2022-06-01 09:04:45 -0600

    add scorekeeping and isKnown logic

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -11,28 +11,48 @@ export function Main() {
   const [currentIdx, setCurrentIdx] = useState(0);
   const currentWord = wordBank.current[currentIdx];
 
-  console.log(
-    `%c ***debug | index.tsx > main`,
-    "background-color: #12908E; color: #f7f7f7; border-radius: 5px; padding: 1em;"
-  );
-  console.log({ currentWord });
+  // scoring state
+  const [totalScore, setTotalScore] = useState(0);
+  const [streak, setStreak] = useState(0);
 
+  // cards hide/show logic
+  const [showCards, setShowCards] = useState(false);
+  const handleIsKnownClick = (isKnown: boolean) => {
+    if (isKnown) {
+      setShowCards(true);
+    } else {
+      setStreak(0);
+      handleNextClick();
+    }
+  };
+
+  // selection/next logic
   const [hasMadeSelection, setHasMadeSelection] = useState(false);
   const handleNextClick = () => {
     setClickedIdx(null);
     setHasMadeSelection(false);
     setCurrentIdx(currentIdx + 1);
+    setShowCards(false);
   };
 
+  // card click logic
   const [clickedIdx, setClickedIdx] = useState<null | number>(null);
   const handleCardClick = (idx: number) => () => {
     if (hasMadeSelection) {
       return;
     }
+    if (idx === currentWord.correctIdx) {
+      setTotalScore(totalScore + 1);
+      setStreak(streak + 1);
+    } else {
+      setStreak(0);
+    }
+
     setHasMadeSelection(true);
     setClickedIdx(idx);
   };
 
+  // halpers
   const getCardVariant = (
     cardIdx: number,
     clickedIdx: number | null,
@@ -48,21 +68,34 @@ export function Main() {
     <Container>
       <Word>{currentWord.word}</Word>
       <CardsContainer>
-        {currentWord.definitions.map((def, idx) => {
-          return (
-            <Card
-              onClick={handleCardClick(idx)}
-              $variant={getCardVariant(idx, clickedIdx, currentWord.correctIdx)}
-              key={`${def}-${idx}`}
-            >
-              {def}
-            </Card>
-          );
-        })}
+        {showCards ? (
+          currentWord.definitions.map((def, idx) => {
+            return (
+              <Card
+                onClick={handleCardClick(idx)}
+                $variant={getCardVariant(
+                  idx,
+                  clickedIdx,
+                  currentWord.correctIdx
+                )}
+                key={`${def}-${idx}`}
+              >
+                {def}
+              </Card>
+            );
+          })
+        ) : (
+          <div>
+            <p onClick={() => handleIsKnownClick(true)}>I know it</p>
+            <p onClick={() => handleIsKnownClick(false)}>I don't know it</p>
+          </div>
+        )}
       </CardsContainer>
       <NextButton disabled={!hasMadeSelection} onClick={handleNextClick}>
         {">>"}
       </NextButton>
+      <p>Score: {totalScore}</p>
+      <p>Streak: {streak}</p>
     </Container>
   );
 }
