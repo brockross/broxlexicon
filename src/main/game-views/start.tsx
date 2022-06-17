ec053f2ceff247023c86bfc61426c37283c105d1
brock ross <brock.ross@dutchie.com>
2022-06-16 22:23:33 -0600

    swap starting buttons

--- src/main/game-views/start.tsx
+++ src/main/game-views/start.tsx
@@ -6,20 +6,20 @@ type StartViewProps = {
 export function StartView({ handleOptionClick }: StartViewProps) {
   return (
     <div className="flex justify-around">
-      <button className="w-1/3 text-center border-2 border-stone-600 rounded-md p-2">
-        <p
-          className="font-body font-light hover:font-medium"
-          onClick={() => handleOptionClick(true)}
-        >
-          I know it
-        </p>
-      </button>
-      <button className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2">
+      <button className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2 text-stone-600">
         <p
           className="font-body font-light  hover:font-medium"
           onClick={() => handleOptionClick(false)}
         >
-          I don't
+          don't know it
+        </p>
+      </button>
+      <button className="w-1/3 text-center border-2 border-stone-500 rounded-md p-2 bg-stone-500 text-stone-200">
+        <p
+          className="font-body font-light hover:font-medium"
+          onClick={() => handleOptionClick(true)}
+        >
+          know it
         </p>
       </button>
     </div>
