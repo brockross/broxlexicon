3f50ab8ff097586915cd62dac7041d406b3b31a1
brock ross <brock.ross@dutchie.com>
2022-07-20 22:36:33 -0600

    move onClicks to button instead of p text

--- src/main/game-views/start.tsx
+++ src/main/game-views/start.tsx
@@ -6,21 +6,17 @@ type StartViewProps = {
 export function StartView({ handleOptionClick }: StartViewProps) {
   return (
     <div className="flex justify-around">
-      <button className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2 text-stone-600">
-        <p
-          className="font-body font-light  hover:font-medium"
-          onClick={() => handleOptionClick(false)}
-        >
-          don't know it
-        </p>
+      <button
+        className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2 text-stone-600"
+        onClick={() => handleOptionClick(false)}
+      >
+        <p className="font-body font-light  hover:font-medium">don't know it</p>
       </button>
-      <button className="w-1/3 text-center border-2 border-stone-500 rounded-md p-2 bg-stone-500 text-stone-200">
-        <p
-          className="font-body font-light hover:font-medium"
-          onClick={() => handleOptionClick(true)}
-        >
-          know it
-        </p>
+      <button
+        className="w-1/3 text-center border-2 border-stone-500 rounded-md p-2 bg-stone-500 text-stone-200"
+        onClick={() => handleOptionClick(true)}
+      >
+        <p className="font-body font-light hover:font-medium">know it</p>
       </button>
     </div>
   );
