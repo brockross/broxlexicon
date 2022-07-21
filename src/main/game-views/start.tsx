2e23ed55d422dfa121a313fe11b87bbad2ad292a
brock ross <brock.ross@dutchie.com>
2022-07-20 22:40:07 -0600

    move hover styles to button instead of p text

--- src/main/game-views/start.tsx
+++ src/main/game-views/start.tsx
@@ -7,16 +7,16 @@ export function StartView({ handleOptionClick }: StartViewProps) {
   return (
     <div className="flex justify-around">
       <button
-        className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2 text-stone-600"
+        className=" w-1/3 text-center border-2 border-stone-600 rounded-md p-2 text-stone-600 font-body font-light hover:font-medium"
         onClick={() => handleOptionClick(false)}
       >
-        <p className="font-body font-light  hover:font-medium">don't know it</p>
+        <p>don't know it</p>
       </button>
       <button
-        className="w-1/3 text-center border-2 border-stone-500 rounded-md p-2 bg-stone-500 text-stone-200"
+        className="w-1/3 text-center border-2 border-stone-500 rounded-md p-2 bg-stone-500 text-stone-200 font-body font-light hover:font-medium"
         onClick={() => handleOptionClick(true)}
       >
-        <p className="font-body font-light hover:font-medium">know it</p>
+        <p>know it</p>
       </button>
     </div>
   );
